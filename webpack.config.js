88cd83e3ada6c21553a6575c520be71bd4f3bfa2
Evgeny Ebergard <eugeneebergard@gmail.com>
2020-05-30 22:08:09 +0500

    step 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   entry: { 
     main: './src/index.js' 
@@ -20,9 +23,9 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use:  [
-          MiniCssExtractPlugin.loader, 
+        test: /\.css$/i,
+        use: [
+          (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
           'css-loader', 
           'postcss-loader'
         ]
@@ -51,11 +54,22 @@ module.exports = {
     new MiniCssExtractPlugin({  
       filename: 'style.[contenthash].css',
     }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash()
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
   ]
 };
